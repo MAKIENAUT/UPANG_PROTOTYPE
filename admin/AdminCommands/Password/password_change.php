5118237c2ad0185c16b41f3014df636816ef553a
MAKIENAUT <mcrayescoto@gmail.com>
2023-03-10 18:37:35 +0800

    PSUHED

--- admin/AdminCommands/Password/password_change.php
+++ admin/AdminCommands/Password/password_change.php
@@ -60,13 +60,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="Admin_Voters.css">
+   <link rel="stylesheet" href="password_change.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
       integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
       crossorigin="anonymous" referrerpolicy="no-referrer" />
    <title>COMELEC ADMIN</title>
    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-   <script type="text/javascript" src="Admin_Voters.js"></script>
+   <script type="text/javascript" src="password_change.js"></script>
 </head>
 
 <body>
