f30a4dcc8f9e0a0bb6c48ee8b4fbeceb6c55ae36
MAKIENAUT <mcrayescoto@gmail.com>
2023-03-10 18:07:56 +0800

    PUSHED

--- admin/AdminCommands/Password/password_change.php
+++ admin/AdminCommands/Password/password_change.php
@@ -2,74 +2,53 @@
 // Initialize the session
 session_start();
 
-session_start();
 require_once "../../../database/database.php";
 
+// Check if user is logged in
 if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
    header('Location: ../../../admin/AdminLogin/admin_login.php');
    exit;
 }
 
+// Check if user has admin clearance
 if ($_SESSION['clearance'] == 'Admin') {
    header('Location: ../../AdminDisplays/AdminDashboard/Admin_Dashboard.php');
    exit;
 }
 
-// Define variables and initialize with empty values
-$new_password = $confirm_password = "";
-$new_password_err = $confirm_password_err = "";
+// Get the user's username
+$username = $_SESSION['username'];
 
-// Processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// Check if form is submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+   // Get the new password from the form
+   $new_password = $_POST['new_password'];
 
-   // Validate new password
-   if (empty(trim($_POST["new_password"]))) {
-      $new_password_err = "Please enter the new password.";
-   } elseif (strlen(trim($_POST["new_password"])) < 6) {
-      $new_password_err = "Password must have atleast 6 characters.";
-   } else {
-      $new_password = trim($_POST["new_password"]);
-   }
+   // Hash the new password using Bcrypt
+   $hashed_password = password_hash($new_password, PASSWORD_BCRYPT);
 
-   // Validate confirm password
-   if (empty(trim($_POST["confirm_password"]))) {
-      $confirm_password_err = "Please confirm the password.";
-   } else {
-      $confirm_password = trim($_POST["confirm_password"]);
-      if (empty($new_password_err) && ($new_password != $confirm_password)) {
-         $confirm_password_err = "Password did not match.";
-      }
-   }
+   // Update the user's password in the database
+   $stmt = $conn->prepare("UPDATE users SET password = ? WHERE username = ?");
+   $stmt->bind_param("ss", $hashed_password, $username);
+   $stmt->execute();
+   $stmt->close();
 
-   // Check input errors before updating the database
-   if (empty($new_password_err) && empty($confirm_password_err)) {
-      // Prepare an update statement
-      $sql = "UPDATE admin SET password = ? WHERE id = ?";
-
-      if ($stmt = mysqli_prepare($conn, $sql)) {
-         // Bind variables to the prepared statement as parameters
-         mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
-
-         // Set parameters
-         $param_password = password_hash($new_password, PASSWORD_DEFAULT);
-         $param_id = $_SESSION["id"];
-
-         // Attempt to execute the prepared statement
-         if (mysqli_stmt_execute($stmt)) {
-            // Password updated successfully. Destroy the session, and redirect to login page
-            session_destroy();
-            header("location: login/login.php");
-            exit();
-         } else {
-            echo "Oops! Something went wrong. Please try again later.";
-         }
-
-         // Close statement
-         mysqli_stmt_close($stmt);
-      }
-   }
-
-   // Close connection
-   mysqli_close($conn);
+   // Redirect to the user's profile page
+   header('Location: profile.php');
+   exit;
 }
 ?>
+
+<html>
+   <head>
+      <title>Reset Password</title>
+   </head>
+   <body>
+      <h1>Reset Password</h1>
+      <form method="post" action="">
+         <label>New Password:</label>
+         <input type="password" name="new_password" required><br><br>
+         <input type="submit" value="Reset Password">
+      </form>
+   </body>
+</html>
