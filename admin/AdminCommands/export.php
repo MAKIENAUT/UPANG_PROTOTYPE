a2c6c715724f80c012892eee97decfaf5dc96a33
MAKIENAUT <mcrayescoto@gmail.com>
2023-03-10 15:21:49 +0800

    PUSHED

--- admin/AdminCommands/export.php
+++ admin/AdminCommands/export.php
@@ -1,28 +1,66 @@
 <?php
+// Database configuration
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'phinma_election';
 
-require_once "../database/database.php";
-// Get the data from the table
-$sql = "SELECT * FROM supreme_candidates";
-$result = $conn->query($sql);
- 
-// Create the header row of the CSV file
-$header = array("id", "Candidate", "Department", "Position", "Votes");
- 
-// Open the file for writing
-$file = fopen("supreme_candidates.csv", "w");
- 
-// Write the header row to the file
-fputcsv($file, $header);
- 
-// Write the data rows to the file
-while ($row = mysqli_fetch_assoc($result)) {
-  fputcsv($file, $row);
+// Tables to export
+$tables = array('admin', 'students', 'supreme_council', 'supreme_government');
+
+// Connect to MySQL server
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
- 
-// Close the file
+
+// Set the filename for the SQL file
+$filename = 'phinma_election_backup_' . date('Y-m-d_H-i-s') . '.sql';
+
+// Open the file for writing
+$file = fopen($filename, 'w');
+
+// Loop through each table
+foreach ($tables as $table_name) {
+    // Get the create statement for the table
+    $create = mysqli_query($conn, "SHOW CREATE TABLE $table_name");
+    $create_row = mysqli_fetch_row($create);
+    $create_statement = $create_row[1];
+    
+    // Write the create statement to the file
+    fwrite($file, $create_statement . ";\n");
+    
+    // Get all data in the table
+    $data = mysqli_query($conn, "SELECT * FROM $table_name");
+    
+    // Get column information
+    $columns = array();
+    $result = mysqli_query($conn, "SHOW COLUMNS FROM $table_name");
+    while ($row = mysqli_fetch_assoc($result)) {
+        if ($row['Extra'] != 'auto_increment') {
+            $columns[] = $row['Field'];
+        }
+    }
+    
+    // Loop through each row and write to the file
+    while ($row = mysqli_fetch_array($data)) {
+        $values = array();
+        foreach ($columns as $column) {
+            $value = mysqli_real_escape_string($conn, $row[$column]);
+            $values[] = "'$value'";
+        }
+        
+        $insert_statement = "INSERT INTO $table_name (" . implode(',', $columns) . ") VALUES (" . implode(',', $values) . ");\n";
+        fwrite($file, $insert_statement);
+    }
+}
+
+// Close the file and database connection
 fclose($file);
- 
-// Close the database connection
 mysqli_close($conn);
+
+echo "Selected tables exported to $filename";
 
 ?>
