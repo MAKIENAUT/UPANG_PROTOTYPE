f61384d3a392d67b99be1665adbbb12ca62547b5
MAKIENAUT <mcrayescoto@gmail.com>
2023-03-10 18:35:10 +0800

    PUSHED

--- admin/AdminCommands/Password/password_change.php
+++ admin/AdminCommands/Password/password_change.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
    $new_password = $_POST['new_password'];
 
    // Check if old password matches the password stored in the database for the user
-   $stmt = $conn->prepare("SELECT password FROM users WHERE username = ?");
+   $stmt = $conn->prepare("SELECT password FROM admin WHERE username = ?");
    $stmt->bind_param("s", $username);
    $stmt->execute();
    $stmt->bind_result($password);
@@ -35,16 +35,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
    if (password_verify($old_password, $password)) {
       // Hash the new password using Bcrypt
-      $hashed_password = password_hash($new_password, PASSWORD_BCRYPT);
+      $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
 
       // Update the user's password in the database
-      $stmt = $conn->prepare("UPDATE users SET password = ? WHERE username = ?");
+      $stmt = $conn->prepare("UPDATE admin SET password = ? WHERE username = ?");
       $stmt->bind_param("ss", $hashed_password, $username);
       $stmt->execute();
       $stmt->close();
 
       // Redirect to the user's profile page
-      header('Location: profile.php');
+      header('Location: ../../AdminDisplay/Admin_Dashboard.php');
       exit;
    } else {
       // If old password is incorrect, show an error message
@@ -53,14 +53,83 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 
-<html>
-   <head>
-      <title>Reset Password</title>
-   </head>
-   <body>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="Admin_Voters.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+      integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <title>COMELEC ADMIN</title>
+   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+   <script type="text/javascript" src="Admin_Voters.js"></script>
+</head>
+
+<body>
+   
+<nav>
+      <div class="nav_head">
+         <div class="user_icon">
+            <img src="../../../photos/Man-Placeholder.png" alt="">
+         </div>
+
+         <div class="user_details">
+            <h2>
+               <?php echo $username; ?>
+            </h2>
+            <h4>Super Admin</h4>
+         </div>
+      </div>
+
+      <div class="nav_title">
+         <h1>APEC</h1>
+         <h3>Election Tally and Result</h3>
+      </div>
+
+      <div class="nav_options">
+         <div class="main_options">
+            <h1>Elective Report</h1>
+            <button class="dashboard" id="dashboard" onclick="show(1)">
+               <div class="button_icon">
+                  <i class="fa-solid fa-gauge"></i>
+               </div>
+               <p>Dashboard</p>
+            </button>
+
+            <button class="result" id="result" onclick="show(2)">
+               <div class="button_icon">
+                  <i class="fa-solid fa-square-poll-vertical"></i>
+               </div>
+               <p>Result</p>
+            </button>
+         </div>
+         <div class="manage_options">
+            <h1>Manage</h1>
+            <button class="voters" id="voters" onclick="show(3)">
+               <div class="button_icon">
+                  <i class="fa-solid fa-people-group"></i>
+               </div>
+               <p>Voters</p>
+            </button>
+            <button class="candidates" id="candidates" onclick="show(4)">
+               <div class="button_icon">
+                  <i class="fa-solid fa-user-plus"></i>
+               </div>
+               <p>Candidates</p>
+            </button>
+         </div>
+      </div>
+   </nav>
+   <main>
       <h1>Reset Password</h1>
       <?php if (isset($error_message)): ?>
-         <p><?php echo $error_message; ?></p>
+         <p>
+            <?php echo $error_message; ?>
+         </p>
       <?php endif; ?>
       <form method="post" action="">
          <label>Old Password:</label>
@@ -69,5 +138,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
          <input type="password" name="new_password" required><br><br>
          <input type="submit" value="Reset Password">
       </form>
-   </body>
+   </main>
+</body>
+
 </html>
